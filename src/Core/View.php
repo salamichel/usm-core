bf2604b83fc31de0b7343d95e6e5f0285228779e
Stéphane <stephane.alamichel@gmail.com>
2026-06-19 17:53:00 +0200

    modif session

--- src/Core/View.php
+++ src/Core/View.php
@@ -38,6 +38,7 @@ class View
             $twig->addGlobal('current_path', parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH) ?? '/');
             $twig->addGlobal('_POST', $_POST);
             $twig->addGlobal('theme', $theme);
+            $twig->addGlobal('app', ['session' => $_SESSION]);
 
             // Contact stats for admin menu badge
             if (Auth::check()) {
