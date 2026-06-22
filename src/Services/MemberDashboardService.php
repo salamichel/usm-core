b36f51b5454fa9396dbadc638a6cbe171278d882
Stéphane <stephane.alamichel@gmail.com>
2026-06-22 18:08:08 +0200

    correction membre dashboard

--- src/Services/MemberDashboardService.php
+++ src/Services/MemberDashboardService.php
@@ -108,7 +108,7 @@ class MemberDashboardService
             $timeDisplay = ($dateObj && $dateObj->format('H:i') !== '00:00') ? $dateObj->format('H:i') : '';
 
             // Participation du joueur
-            $statusStr = $m['ma_participation'] ?? '';
+            $statusStr = $normalized['ma_participation'] ?? '';
             $status = new ParticipationStatus($statusStr);
             $category = $status->getCategory();
 
